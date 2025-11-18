baf8d2b4d10fdc95d46c9b794e8c841ba7242b5c
baranck0513 <barancemkaramanli4@gmail.com>
2025-11-18 15:07:07 +0000

    impletemented exit

--- app/main.py
+++ app/main.py
@@ -6,10 +6,9 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
-        if command == "exit":
-            sys.exit(0)
-        else:
-            print(f"{command}: command not found")
+        if command == "exit 0":
+            exit()
+        print(f"{command}: command not found")
         # pass
 
 
