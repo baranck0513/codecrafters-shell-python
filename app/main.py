15f2e7494f7ac64a9d10d006b74adfec7f3fe6ea
baranck0513 <barancemkaramanli4@gmail.com>
2025-11-18 15:14:14 +0000

    implemented echo

--- app/main.py
+++ app/main.py
@@ -2,13 +2,22 @@ import sys
 
 
 def main():
-    # TODO: Uncomment the code below to pass the first stage
     while True:
         sys.stdout.write("$ ")
         command = input()
         if command == "exit 0":
             exit()
+
+        if command.startswith("echo "):
+            print(command[5:])
+            continue
+        if command == "echo":
+            print("")
+            continue
+
         print(f"{command}: command not found")
+
+    
         # pass
 
 
